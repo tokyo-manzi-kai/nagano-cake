b629dcff6378a8542412faa74d0426e4868cfde9
EC2 Default User <ec2-user@ip-172-31-41-169.ec2.internal>
2022-03-17 07:11:40 +0000

    header編集

--- config/routes.rb
+++ config/routes.rb
@@ -15,65 +15,31 @@ Rails.application.routes.draw do
   #管理者
   namespace :admins do
     resources :order_details, only: [:update]
-    # get 'order_details/update'
     resources :orders, only: [:show, :update]
-    # get 'orders/show'
-    # get 'orders/update'
-     resources :customers, only: [:index, :show, :edit, :update]
-    # get 'customers/index'
-    # get 'customers/show'
-    # get 'customers/edit'
-    # get 'customers/update'
+    resources :customers, only: [:index, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
-    # get 'genres/index'
-    # get 'genres/create'
-    # get 'genres/edit'
-    # get 'genres/update'
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
-    # get 'items/index'
-    # get 'items/new'
-    # get 'items/create'
-    # get 'items/show'
-    # get 'items/edit'
-    # get 'items/update'
     get 'homes/top'
   end
 
   # 顧客
   scope module: :end_users do
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-    # get 'addresses/index'
-    # get 'addresses/edit'
-    # get 'addresses/create'
-    # get 'addresses/update'
-    # get 'addresses/destroy'
     resources :orders, only: [:new, :create, :index, :show] do
       collection do
         post 'orders/comfilm'
         get 'orders/complete'
       end
     end
-    # get 'orders/new'
-    # get 'orders/create'
-    # get 'orders/index'
-    # get 'orders/show'
     resources :cart_items, only: [:index]
-    # get 'cart_items/index'
     resources :users, only: [:show, :edit, :update] do
       collection do
         get 'users/unsubscribe'
         patch 'users/withdraw'
       end
     end
-    # get 'users/show'
-    # get 'users/edit'
-    # get 'users/update'
     resources :registrations, only: [:new, :create]
-    # get 'registrations/new'
-    # get 'registrations/create'
     resources :items, only: [:index, :show]
-    # get 'items/index'
-    # get 'items/show'
     root to: 'homes#top'
     get 'homes/about'
   end
