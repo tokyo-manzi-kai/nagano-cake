881ae563409b1ce21cc715debd833eeaaf6cdf5c
EC2 Default User <ec2-user@ip-172-31-91-77.ec2.internal>
2022-03-18 10:43:05 +0000

    model作成

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -1,6 +1,12 @@
 class EndUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :shipping_addresses
+  has_many :orders
+  has_many :cart_items
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
 end
