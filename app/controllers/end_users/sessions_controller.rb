a4cdf4443c2294e11e4c36408f87eebda140903b
EC2 Default User <ec2-user@ip-172-31-84-216.ec2.internal>
2022-03-21 05:54:25 +0000

    退会機能修正

--- app/controllers/end_users/sessions_controller.rb
+++ app/controllers/end_users/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class EndUsers::SessionsController < Devise::SessionsController
+
+  before_action :end_user_state, only: [:create]
   # before_action :reject_inactive_enduser, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
@@ -19,27 +21,18 @@ class EndUsers::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-    # def end_user_state
-    #   @end_user = EndUser.find_by(email: params[:end_user][:email])
-    #   return if !@end_user
-    #     if @end_user.valid_password?(params[:end_user][:password]) && (@end_user.withdraw == true)
-    #     redirect_to new_end_user_session_path
-    #     end
-    # end
-    
-    # def reject_inactive_enduser
-    #   @end_user = EndUser.find_by(name: params[:user][:name])
-    #   if @end_user
-    #     if @end_user.valid_password?(params[:user][:password]) && !@end_user.is_valid
-    #       redirect_to new_end_user_session_path
-    #     end
-    #   end
-    # end
-    
+
+  def end_user_state
+    @end_user = EndUser.find_by(email: params[:end_user][:email])
+    return if !@end_user
+    if @end_user.valid_password?(params[:end_user][:password]) && @end_user.is_deleted
+      redirect_to new_end_user_session
+    end
+  end
 end
