95456711d39108df37898a6239ec4861a636cad4
EC2 Default User <ec2-user@ip-172-31-41-169.ec2.internal>
2022-03-24 05:57:26 +0000

    ジャンル

--- app/controllers/end_users/items_controller.rb
+++ app/controllers/end_users/items_controller.rb
@@ -2,10 +2,16 @@ class EndUsers::ItemsController < ApplicationController
   before_action :authenticate_end_user!, except: [:top, :about, :index]
 
   def index
-    @items = Item.where(is_active: "true").page(params[:page]).per(8)
+    @genres = Genre.all
+    @items = Item.all
+    if params[:genre_id] != nil
+      @items = @items.where(genre_id: params[:genre_id])
+    end
+    @items = @items.where(is_active: "true").page(params[:page]).per(8)
   end
 
   def show
+    @genres = Genre.all
     @items = Item.find(params[:id])
     @cart_items = CartItem.new
   end
