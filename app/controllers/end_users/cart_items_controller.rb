8eb65d483d70551d72bd6f234690e242dbdd6f11
EC2 Default User <ec2-user@ip-172-31-41-169.ec2.internal>
2022-03-25 08:33:39 +0000

    最終

--- app/controllers/end_users/cart_items_controller.rb
+++ app/controllers/end_users/cart_items_controller.rb
@@ -11,7 +11,7 @@ class EndUsers::CartItemsController < ApplicationController
     redirect_to request.referer
   end
 
-  def destro
+  def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
     redirect_to request.referer
